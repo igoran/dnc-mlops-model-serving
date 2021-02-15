c3cfeb23868e1827b803c56cadbbafcb0b901e71
Luca Milan <luca.milan@gmail.com>
2021-02-15 17:24:20 +0100

    Updated metrics

--- ml/Predictor/Services/ApplicationInsightsMetricsClient.cs
+++ ml/Predictor/Services/ApplicationInsightsMetricsClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Logging;
@@ -10,11 +11,13 @@ namespace Predictor.Services
     public class ApplicationInsightsClient : IMetricsClient
     {
         private readonly TelemetryClient _telemetryClient;
+        private readonly Uri _modelUri;
 
         public ApplicationInsightsClient(TelemetryConfiguration telemetryConfiguration, Uri modelUri)
         {
             _telemetryClient = new TelemetryClient(telemetryConfiguration);
-            _telemetryClient.Context.Operation.Name = modelUri.ToString();
+            _telemetryClient.Context.Operation.Name = modelUri?.Segments?.LastOrDefault()?.ToString();
+            _modelUri = modelUri;
         }
 
         public void Track(SentimentPrediction prediction, SentimentIssue data, ILogger logger)
@@ -26,14 +29,16 @@ namespace Predictor.Services
                 var props = new Dictionary<string, string>
                 {
                     { "model_name", Constants.ModelName },
+                    { "model_version", _modelUri?.Segments?.LastOrDefault()?.ToString() },
+                    { "model_uri", _modelUri?.ToString() },
                     { "text", sentimentText },
                 };
+
+                _telemetryClient.TrackMetric("Prediction.Response", prediction.Sentiment ? 1 : 0, props);
 
                 _telemetryClient.TrackMetric("Prediction.Probability", prediction.Probability, props);
 
                 _telemetryClient.TrackMetric("Prediction.Score", prediction.Score, props);
-
-                logger?.LogInformation($"Metrics updated. Score: {prediction.Score}");
             }
             catch(Exception ex)
             {
